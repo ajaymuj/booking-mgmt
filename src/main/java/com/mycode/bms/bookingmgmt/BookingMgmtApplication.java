f4836e26f135d54c0099acb5c419c5d522ff8e6a
Ajay Mujumdar <ajay.mujumdar@oracle.com>
2022-01-09 18:19:41 +0530

    First commit

---
+++ src/main/java/com/mycode/bms/bookingmgmt/BookingMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.mycode.bms.bookingmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycode/bms/bookingmgmt/BookingMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycode.bms.bookingmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
